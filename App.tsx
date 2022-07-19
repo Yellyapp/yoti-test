cc1ffeb76cd95c50778534b8aa59890e0fa52c37
Kanstantsin Kiyko <kanstantsin.kiyko@callstack.com>
2022-07-19 20:32:19 +0400

    chore: update the yoti-face-capture and enchance the demo app

--- App.tsx
+++ App.tsx
@@ -1,47 +1,37 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {
   PixelRatio,
   Text,
-  TouchableOpacity,
+  StyleSheet,
+  Platform,
   useWindowDimensions,
   View,
+  Button,
+  SafeAreaView
 } from "react-native";
-import { Camera, CameraType } from "expo-camera";
-// Image quality options
+import {Camera} from "expo-camera";
 import YotiFaceCapture from "@getyoti/react-native-yoti-face-capture";
 
 console.disableYellowBox = true;
 export default function App() {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-  const [image, setImage] = useState<any>(null);
+  const [message, setMessage] = useState<string | null>(null)
+  const [isAnalyzing, setIsAnalyzing] = useState<boolean>(false)
+  const [isCaptured, setIsCaptured] = useState<boolean>(false)
+
   const yotiFaceCaptureRef = useRef(null);
   const windowHeight = useWindowDimensions().height;
   const windowWidth = useWindowDimensions().width;
 
-  // You can then control the camera and analysis using the ref
-
-  // Start the camera
-  // yotiFaceCaptureRef.current.startCamera()
-
-  // Start the analysis (having started the camera)
-  // yotiFaceCaptureRef.current.startAnalysis()
-
-  // Stop the analysis
-  // yotiFaceCaptureRef.current.stopAnalysis()
-
-  // Stop the camera
-  // yotiFaceCaptureRef.current.stopCamera()
   useEffect(() => {
     if (hasPermission) {
       yotiFaceCaptureRef.current.startCamera();
-      yotiFaceCaptureRef.current.startAnalyzing();
     }
   }, [hasPermission]);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-
+      const {status} = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === "granted");
     })();
   }, []);
@@ -52,48 +42,102 @@ export default function App() {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+  const topOffset = Platform.select({android: 60, default: 60})
+
+  const params = Platform.select({
+    // Looks strange but it works for some reason
+    // Without this hack android won't recognize the face from the camera
+    // Uncomment for working android version
+    
+    //android: {
+    // x: 0,
+    // y: windowWidth / 2 + topOffset,
+    // width: windowWidth * 2,
+    // height: windowHeight + topOffset * 2,
+    // },
+    default: {
+      x: 0,
+      y: topOffset,
+      width: windowWidth,
+      height: windowHeight - topOffset,
+    },
+    ios:{
+      x: 0,
+      y: PixelRatio.getPixelSizeForLayoutSize(topOffset),
+      width: PixelRatio.getPixelSizeForLayoutSize(windowWidth),
+      height: PixelRatio.getPixelSizeForLayoutSize(windowHeight - topOffset),
+    }
+  })
+  const area = [
+    PixelRatio.roundToNearestPixel(params.x),
+    PixelRatio.roundToNearestPixel(params.y),
+    PixelRatio.roundToNearestPixel(params.width),
+    PixelRatio.roundToNearestPixel(params.height),
+  ]
+
+  const switchAnalysis = (flag) => {
+    if(flag){
+      yotiFaceCaptureRef.current.startAnalyzing()
+    } else {
+      yotiFaceCaptureRef.current.stopAnalyzing()
+    }
+    setIsAnalyzing(flag)
+    setMessage('')
+    setIsCaptured(false)
+  } 
+
   return (
-    <YotiFaceCapture
-      style={{ flex: 1, width: "100%" }}
-      ref={yotiFaceCaptureRef}
-      requireEyesOpen={false}
-      requiredStableFrames={0}
-      requireValidAngle={false}
-      requireBrightEnvironment
-      scanningArea={[
-        0,
-        0,
-        PixelRatio.getPixelSizeForLayoutSize(windowWidth),
-        PixelRatio.getPixelSizeForLayoutSize(windowHeight),
-      ]}
-      onFaceCaptureAnalyzedImage={(e) => {
-        console.log("onFaceCaptureAnalyzedImage", e);
-
-        // analysis.croppedImage
-        // analysis.croppedFaceBoundingBox
-        // analysis.faceBoundingBox
-        // analysis.originalImage
-      }}
-      onFaceCaptureImageAnalysisFailed={(e) => {
-        console.log("onFaceCaptureImageAnalysisFailed", e.cause);
-
-        if (e.cause === "FaceCaptureAnalysisErrorFaceNotCentered") {
-          //yotiFaceCaptureRef.current.stopAnalyzing();
-        }
-        // failure.cause
-        // failure.originalImage
-      }}
-      onFaceCaptureStateChanged={(e) => {
-        console.log("onFaceCaptureStateChanged", e);
-
-        // state may either be 'Analyzing', 'CameraReady' or 'CameraStopped'
-      }}
-      onFaceCaptureStateFailed={(e) => {
-        console.log("onFaceCaptureStateFailed", e);
-        // failure.cause
-        // failure may either be 'CameraInitializationError' or 'MissingPermissions'
-      }}
-    />
+    <>
+      <YotiFaceCapture
+        style={{flex: 1, width: "100%"}}
+        ref={yotiFaceCaptureRef}
+        requireEyesOpen={false}
+        requiredStableFrames={0}
+        requireBrightEnvironment
+        scanningArea={area}
+        onFaceCaptureAnalyzedImage={(e) => {
+          if(!isCaptured) {
+            console.log("onFaceCaptureAnalyzedImage", 'Face capture success');
+            switchAnalysis(false)
+            setMessage('SUCCESS')
+            setIsCaptured(true)
+          }
+        }}
+        onFaceCaptureImageAnalysisFailed={(e) => {
+          console.log("onFaceCaptureImageAnalysisFailed", e.cause);
+          setMessage(e.cause)
+        }}
+        onFaceCaptureStateChanged={(e) => {
+          console.log("onFaceCaptureStateChanged", e);
+        }}
+        onFaceCaptureStateFailed={(e) => {
+          console.log("onFaceCaptureStateFailed", e);
+      }} />
+      <SafeAreaView>
+        <Button color="red" onPress={()=>{ switchAnalysis(!isAnalyzing)}} title={isAnalyzing ? 'Stop' : 'Start'}></Button>
+      </SafeAreaView>
+      <View style={styles.messageWrapper}>
+        <Text style={styles.message}>
+          {message}
+        </Text>
+      </View>
+    </>
   );
 }
 
+const styles = StyleSheet.create({
+  messageWrapper: {
+    zIndex: 2,
+    elevation: 2,
+    position: 'absolute',
+    bottom: 100,
+    left: 10,
+    right: 10,
+    backgroundColor: 'rgba(100,100,100,0.3)',
+    borderRadius: 40,
+  },
+  message: {
+    padding: 10,
+    textAlign: 'center'
+  }
+})
